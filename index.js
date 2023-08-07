c06fad4ef72384d915df23ca86d4920fd4e68a8b
Tony W <bobacrypto2@gmail.com>
2023-08-07 00:29:54 -0700

    Basic Informational Site can now navigate through different .html files all with just NodeJS

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const http = require("http");
+const url = require("url");
+const fs = require("fs");
 
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.write(req.url);
-  res.end();
+  let q = url.parse(req.url, true);
+  let fileName = `.${q.pathname}.html`;
+
+  if (q.pathname === "/") {
+    fileName = "./index.html";
+  }
+
+  fs.readFile(`${fileName}`, (err, data) => {
+    if (err) {
+      fs.readFile("./404.html", (err, data) => {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    }
+  });
 });
 
 server.listen(port, () => {
