f9281c3a87d8d6a0e25123f32ce85f37067dab20
Tony W <bobacrypto2@gmail.com>
2023-08-15 01:25:40 -0700

    Rewrite all html pages into Pug and convert to Pug syntax from HTML

--- index.js
+++ index.js
@@ -10,36 +10,3 @@ app.get("/", (req, res) => {
 });
 
 app.listen(3000);
-
-// const http = require("http");
-// const url = require("url");
-// const fs = require("fs");
-
-// const port = 8080;
-
-// const server = http.createServer((req, res) => {
-//   let q = url.parse(req.url, true);
-//   let fileName = `.${q.pathname}.html`;
-
-//   if (q.pathname === "/") {
-//     fileName = "./index.html";
-//   }
-
-//   fs.readFile(`${fileName}`, (err, data) => {
-//     if (err) {
-//       fs.readFile("./404.html", (err, data) => {
-//         res.writeHead(404, { "Content-Type": "text/html" });
-//         res.write(data);
-//         return res.end();
-//       });
-//     } else {
-//       res.writeHead(200, { "Content-Type": "text/html" });
-//       res.write(data);
-//       return res.end();
-//     }
-//   });
-// });
-
-// server.listen(port, () => {
-//   console.log(`Server listening to ${port}`);
-// });
