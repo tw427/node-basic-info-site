5fd33f33db37edbba27f2bdeeab440d96573136b
Tony W <bobacrypto2@gmail.com>
2023-08-15 01:37:10 -0700

    Create routes to about and contact me pages

--- index.js
+++ index.js
@@ -9,4 +9,12 @@ app.get("/", (req, res) => {
   res.render("index", { message: "Hello Pug!" });
 });
 
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/contact-me", (req, res) => {
+  res.render("contact-me");
+});
+
 app.listen(3000);
